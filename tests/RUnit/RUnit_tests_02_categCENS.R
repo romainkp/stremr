3ae175ac358303cf376784a7145b231cd729d834
Oleg Sofrygin <olegso@gmail.com>
2016-04-06 16:12:49 -0700

    update tests for categor cens

--- tests/RUnit/RUnit_tests_02_categCENS.R
+++ tests/RUnit/RUnit_tests_02_categCENS.R
@@ -1,9 +1,18 @@
-set.DAG.rm.template <- function(catC = FALSE){
-  require(simcausal)
+simulateDATA.fromDAG <- function(catC = FALSE, Nsize = 1000, rndseed = NULL){
+  library("simcausal")
   options(simcausal.verbose = FALSE)
   rcategor.int.b0 <- function(n, probs) { rcategor.int(n, probs)-1 }
+  # get the likelihood under N(t) Bernoulli with P(N(t)=1)=p:
+  g.p <- function(p){
+    function(O.data, Yname){
+      g.N <- rep(p, nrow(O.data))
+      g.N <- ifelse(O.data[, "N.t"] == 1, g.N, 1 - g.N) # will make it NA for rows when Y.t=1
+      return(g.N)
+    }
+  }
+
   Drm <- DAG.empty()
-###### L(0)=(Z(0),I*(0)) - I*(m) represents value of test at m-1 if test performed: I*(m)=I(m)*T(m-1) where I(m) is value of test (possibly unobserved)
+
   Drm <- Drm +
     node("Y", t = 0, distr = "rbern", prob = 0, EFU = TRUE) +  #Z(0)
     node("lastNat1", t = 0, distr = "rconst", const = 0) +  #Z(0) - see below for definition,  set at 0 at t = 0 by convention (see also below)
@@ -11,18 +20,18 @@ set.DAG.rm.template <- function(catC = FALSE){
     node("highA1c", t = 0, distr = "rbern", prob = highA1c.UN[0]) +  # I*(0) = I(0)*T(-1) with T(-1) = 1 by convention - all patient come with an A1c value known - T(-1) is what I call N(-1)
     node("CVD", t = 0, distr = "rbern", prob = ifelse(highA1c[0] == 1, 0.5, 0.1)) +  #Z(0)
     node("timelowA1c.UN", t = 0, distr = "rbern", prob = 1-highA1c.UN[0]) +  # Z(0) - counts the number of time the (possibly unobserved) A1c was low
-###### A(0)
+
     node("TI", t = 0, distr = "rbern", prob = ifelse(highA1c[0] == 0, ifelse(CVD[0] == 1, 0.5, 0.1), ifelse(CVD[0] == 1, 0.9, 0.5))) +
     node("C", t = 0, distr = "rbern", prob = 0, EFU = TRUE) +  # no censoring in first bin
-###### T(0)
+
     node("N", t = 0, distr = "rbern", prob = 1) +
-###### L(t) for t = 1, ..., 16
+
     node("Y", t = 1:16, distr = "rbern", prob = plogis(-6.5 + 1*CVD[0] + 4*highA1c.UN[t-1] + 0.05*timelowA1c.UN[t-1]),  EFU = TRUE) +  # Z(t)
     node("lastNat1", t = 1:16, distr = "rconst", const = ifelse(N[t-1] == 0, lastNat1[t-1] + 1, 0)) +  # Z(1)  just a function of past \bar{N}(t-1) - 0 probs current N at 1,  1 probs previous N a 1,  2 probs  the one before the previous was at 1,  etc.
     node("highA1c.UN", t = 1:16, distr = "rbern", prob = ifelse(TI[t-1] == 1, 0.1, ifelse(highA1c.UN[t-1] == 1, 0.9, min(1, 0.1 + t/16)))) +  # I(t)
     node("highA1c", t = 1:16, distr = "rbern", prob = ifelse(N[t-1] == 1, highA1c.UN[t], highA1c[t-1])) + # I*(m)=I(m)*T(m-1)  (I actually replace I*(m)=0 with when T(m-1)=0 with last value carried forward,  i.e. I*(m-1)
     node("timelowA1c.UN", t=1:16, distr="rnorm", mean=sum(1-highA1c.UN[0:t]),  sd=0) +  # Z(m)
-###### A(t)
+
     node("TI", t = 1:16, distr = "rbern",
       prob =
         ifelse(TI[t-1] == 1, 1,
@@ -35,9 +44,20 @@ set.DAG.rm.template <- function(catC = FALSE){
     node("N", t = 1:16, distr = "rbern", prob = 1)
 
   if (catC) {
-    Drm <- Drm + node("C", t = 1:16, distr = "rcategor.int.b0", probs = c(0.95, 0.025, 0.025), EFU = TRUE)
+    Drm <- Drm + node("C", t = 1:16, distr = "rcategor.int.b0", probs = c(0.90, 0.05, 0.05), EFU = TRUE)
   }
-  return(Drm)
+  # return(Drm)
+
+  g05.Params = list(name = "Sporadic 0.5", gInt.N = g.p(0.5), Nprob.t0 = 0.5, Nprob.tplus = 0.5)
+  prob.t0 <- g05.Params$Nprob.t0
+  prob.tplus <- g05.Params$Nprob.tplus
+  if (!is.null(prob.t0)) {
+    Drm <- Drm + node("N", t = 0, distr = "rbern", prob = .(prob.t0))
+    Drm <- Drm + node("N", t = 1:16, distr = "rbern", prob = .(prob.tplus))
+  }
+  DAGobj <- set.DAG(Drm, latent.v = c("highA1c.UN", "timelowA1c.UN"))
+  O.data <- sim(DAG = DAGobj, n = Nsize, wide = FALSE, rndseed = rndseed)
+  return(O.data)
 }
 
 test.model.fits.categorCENSOR <- function() {
@@ -46,17 +66,7 @@ test.model.fits.categorCENSOR <- function() {
   # ------------------------------------------------------------------------------------------------------
   Nsize <- 50000
   library("simcausal")
-  DAGrm <- set.DAG.rm.template(catC=TRUE)
-
-  g05.Params = list(name = "Sporadic 0.5", gInt.N = g.p(0.5), Nprob.t0 = 0.5, Nprob.tplus = 0.5)
-  prob.t0 <- g05.Params$Nprob.t0
-  prob.tplus <- g05.Params$Nprob.tplus
-  if (!is.null(prob.t0)) {
-    DAGrm <- DAGrm + node("N", t = 0, distr = "rbern", prob = .(prob.t0))
-    DAGrm <- DAGrm + node("N", t = 1:16, distr = "rbern", prob = .(prob.tplus))
-  }
-  DAG <- set.DAG(DAGrm)
-  O.data <- sim(DAG = DAG, n = Nsize, wide = FALSE, rndseed = 124356)
+  O.data <- simulateDATA.fromDAG(Nsize = Nsize, rndseed = 124356, catC=TRUE)
   O.data[O.data[,"t"]%in%16,"lastNat1"] <- NA
   O.data <- O.data[,!names(O.data)%in%c("highA1c.UN", "timelowA1c.UN")]
   head(O.data)
