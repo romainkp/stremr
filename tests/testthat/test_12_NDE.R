cfe5c33bf799c0350675b185e48a1a35544d7ff3
Oleg Sofrygin <olegso@gmail.com>
2017-06-15 18:26:08 -0700

    trying to fix NDE tests again

--- tests/testthat/test_12_NDE.R
+++ tests/testthat/test_12_NDE.R
@@ -57,17 +57,17 @@ test_that("IPW-KM with stochastic intervention on MONITOR", {
   surv1.stoch <- survNPMSM(wts.St.dlow, OData)
   expect_true(
   all.equal(
-    paste0(surv1.stoch[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.985853098100471,0.985853098100471,0.985853098100471,0.985853098100471,0.985853098100471,0.978011271542912,0.978011271542912,0.411379895630615,0.411379895630615,0.411379895630615,0.411379895630615,0.411379895630615,0.40998903378949,0.40998903378949,0.409988999668537,0.409988999668537,NaN")
+    paste0(round(surv1.stoch[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9859,0.9859,0.9859,0.9859,0.9859,0.978,0.978,0.4114,0.4114,0.4114,0.4114,0.4114,0.41,0.41,0.41,0.41,NaN")
   )
 
   wts.St.dhigh <- getIPWeights(OData, intervened_TRT = "gTI.dhigh", intervened_MONITOR = "gPois3.yrly")
   surv2.stoch <- survNPMSM(wts.St.dhigh, OData)
-  surv2.stoch[["estimates"]][["St.NPMSM"]]
+  # surv2.stoch[["estimates"]][["St.NPMSM"]]
   expect_true(
   all.equal(
-    paste0(surv2.stoch[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.980715607311163,0.974547581455095,0.974547581455095,0.930255027709849,0.926715229154425,0.848554097090501,0.846450767459393,0.734914128610553,0.734914128610553,0.594839291790172,0.594839291790172,0.581772042885852,0.581772042885852,0.581772042885852,0.581772042885852,0.581772042885852,NaN")
+    paste0(round(surv2.stoch[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9807,0.9745,0.9745,0.9303,0.9267,0.8486,0.8465,0.7349,0.7349,0.5948,0.5948,0.5818,0.5818,0.5818,0.5818,0.5818,NaN")
   )
 })
 
@@ -77,16 +77,16 @@ test_that("IPW-KM with static intervention on MONITOR", {
   surv1.stat <- survNPMSM(wts.St.dlow, OData)
   expect_true(
   all.equal(
-    paste0(surv1.stat[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.985593308172962,0.985593308172962,0.985593308172962,0.985593308172962,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
+    paste0(round(surv1.stat[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9856,0.9856,0.9856,0.9856,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
   )
 
   wts.St.dhigh <- getIPWeights(OData, intervened_TRT = "gTI.dhigh", intervened_MONITOR = "N.star.0101")
   surv2.stat <- survNPMSM(wts.St.dhigh, OData)
   expect_true(
   all.equal(
-    paste0(surv2.stat[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.979867574342635,0.94185134182697,0.94185134182697,0.811389084742802,0.811389084742802,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
+    paste0(round(surv2.stat[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9799,0.9419,0.9419,0.8114,0.8114,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
   )
 })
 
@@ -100,16 +100,16 @@ test_that("IPW-KM with static intervention on MONITOR under NDE assumption", {
   surv1.statNDE <- survNPMSM(wts.St.dlow, OData)
   expect_true(
   all.equal(
-    paste0(surv1.statNDE[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,0.98964898762371,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
+    paste0(round(surv1.statNDE[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9896,0.9896,0.9896,0.9896,0.9896,0.9896,0.9896,0.9896,0.9896,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
   )
 
   wts.St.dhigh <- getIPWeights(OData, intervened_TRT = "gTI.dhigh", intervened_MONITOR = "N.star.0101", useonly_t_MONITOR = "N.star.0101 == 1")
   surv2.statNDE <- survNPMSM(wts.St.dhigh, OData)
   expect_true(
   all.equal(
-    paste0(surv2.statNDE[["estimates"]][["St.NPMSM"]], collapse = ","),
-    "0.989613184270603,0.970013783172476,0.970013783172476,0.936567895363756,0.936567895363756,0.936567895363756,0.936567895363756,0.618726872904148,0.618726872904148,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
+    paste0(round(surv2.statNDE[["estimates"]][["St.NPMSM"]],4), collapse = ","),
+    "0.9896,0.97,0.97,0.9366,0.9366,0.9366,0.9366,0.6187,0.6187,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN")
   )
 
 })
@@ -124,8 +124,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR", {
 
   expect_true(
   all.equal(
-    paste0(gcomp_est3[["estimates"]][["St.GCOMP"]], collapse = ","),
-    "0.989999999994813,0.98351871714058,0.964300896726698,0.901124778851299")
+    paste0(round(gcomp_est3[["estimates"]][["St.GCOMP"]],4), collapse = ","),
+    "0.99,0.9835,0.9643,0.9011")
   )
 
   # stratified modeling by rule followers only:
@@ -135,8 +135,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR", {
 
   expect_true(
   all.equal(
-    paste0(tmle_est3[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.9900000000139,0.980512185135154,0.971945831548481,0.952551647600147")
+    paste0(round(tmle_est3[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9805,0.9719,0.9526")
   )
 
   # pooling all observations (no stratification):
@@ -146,8 +146,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR", {
 
   expect_true(
   all.equal(
-    paste0(tmle_est4[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.990000000017604,0.980527554222798,0.973488109369388,0.955062511868786")
+    paste0(round(tmle_est4[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9805,0.9735,0.9551")
   )
 
 })
@@ -161,8 +161,8 @@ test_that("TMLE / GCOMP with a static intervention on MONITOR under NDE assumpti
                             useonly_t_MONITOR = "N.star.0101 == 1", Qforms = Qforms, stratifyQ_by_rule = FALSE)
   expect_true(
   all.equal(
-    paste0(gcomp_est3[["estimates"]][["St.GCOMP"]], collapse = ","),
-    "0.989999999994757,0.967939608547372,0.961609342301288,0.883919336536042")
+    paste0(round(gcomp_est3[["estimates"]][["St.GCOMP"]],4), collapse = ","),
+    "0.99,0.9679,0.9616,0.8839")
   )
 
   ## stratified modeling by rule followers only:
@@ -171,8 +171,8 @@ test_that("TMLE / GCOMP with a static intervention on MONITOR under NDE assumpti
                         useonly_t_MONITOR = "N.star.0101 == 1", Qforms = Qforms, stratifyQ_by_rule = TRUE)
   expect_true(
   all.equal(
-    paste0(tmle_est3[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.990000000013414,0.964650728489762,0.943901699485709,0.933132156613137")
+    paste0(round(tmle_est3[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9647,0.9439,0.9331")
   )
 
   # pooling all observations (no stratification):
@@ -180,8 +180,8 @@ test_that("TMLE / GCOMP with a static intervention on MONITOR under NDE assumpti
                         useonly_t_MONITOR = "N.star.0101 == 1", Qforms = Qforms, stratifyQ_by_rule = FALSE)
   expect_true(
   all.equal(
-    paste0(tmle_est4[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.990000000017421,0.965155755596925,0.950465241673256,0.869326959247925")
+    paste0(round(tmle_est4[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9652,0.9505,0.8693")
   )
 
 })
@@ -194,8 +194,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR under NDE assu
                             useonly_t_MONITOR = "gPois3.yrly == 1", Qforms = Qforms, stratifyQ_by_rule = FALSE)
   expect_true(
   all.equal(
-    paste0(gcomp_est3[["estimates"]][["St.GCOMP"]], collapse = ","),
-    "0.989999999994757,0.977605017200815,0.96640066868087,0.900457218199797")
+    paste0(round(gcomp_est3[["estimates"]][["St.GCOMP"]],4), collapse = ","),
+    "0.99,0.9776,0.9664,0.9005")
   )
 
   # stratified modeling by rule followers only:
@@ -203,8 +203,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR under NDE assu
                         useonly_t_MONITOR = "gPois3.yrly == 1", Qforms = Qforms, stratifyQ_by_rule = TRUE)
   expect_true(
   all.equal(
-    paste0(tmle_est3[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.990000000013414,0.975704302751518,0.964657999663891,0.884770719616591")
+    paste0(round(tmle_est3[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9757,0.9647,0.8848")
   )
 
   # pooling all observations (no stratification):
@@ -212,8 +212,8 @@ test_that("TMLE / GCOMP with a stochastic intervention on MONITOR under NDE assu
                         useonly_t_MONITOR = "gPois3.yrly == 1", Qforms = Qforms, stratifyQ_by_rule = FALSE)
   expect_true(
   all.equal(
-    paste0(tmle_est4[["estimates"]][["St.TMLE"]], collapse = ","),
-    "0.990000000017421,0.975597334629591,0.967482584420614,0.887221610893926")
+    paste0(round(tmle_est4[["estimates"]][["St.TMLE"]],4), collapse = ","),
+    "0.99,0.9756,0.9675,0.8872")
   )
 
 })
