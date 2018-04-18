22322857446ab67697a3f8c519bbfb4150b18deb
Oleg Sofrygin <olegso@gmail.com>
2018-04-18 11:09:57 -0700

    adding option for stabilized weights summary

--- R/helper_routines.R
+++ R/helper_routines.R
@@ -199,8 +199,9 @@ get_FUPtimes <- function(wts_data, IDnode, tnode) {
 #' @return A list with various IP-weights summary tables.
 #' @seealso \code{\link{getIPWeights}} for evaluation of IP-weights.
 #' @export
-get_wtsummary <- function(wts_data, cutoffs = c(0, 0.5, 1, 10, 20, 30, 40, 50, 100, 150), varname = "Stabilized IPAW", by.rule = FALSE) {
+get_wtsummary <- function(wts_data, cutoffs = c(0, 0.5, 1, 10, 20, 30, 40, 50, 100, 150), varname = "Stabilized IPAW", by.rule = FALSE, stabilize=FALSE) {
   wts_data <- format_wts_data(wts_data)
+  if (stabilize) wts_data[, `:=`("cum.IPAW", cum.stab.P * cum.IPAW)]
   # get the counts for each category (bin) + missing count:
   na.count <- sum(is.na(wts_data[["cum.IPAW"]]))
   na.yes <- (na.count > 0L)
